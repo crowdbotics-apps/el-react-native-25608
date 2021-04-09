bc1dad8a474d745ff1661d4e47e5c9b7d1a4786e
Crowdbotics <team@crowdbotics.com>
2021-04-09 23:01:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/el_react_native_25608/MainActivity.java
@@ -0,0 +1,15 @@
+package com.el_react_native_25608;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "el_react_native_25608";
+  }
+}
